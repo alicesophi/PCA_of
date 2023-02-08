52b2feba3561797a5d1d7d9fdb7e5deb096dea26
alicesophi <120331753+alicesophi@users.noreply.github.com>
2023-02-08 01:22:43 -0400

    Update ex2.c

--- aula 26.01/ex2.c
+++ aula 26.01/ex2.c
@@ -1,22 +1,30 @@
+/*Criar uma função que efetue e apresente o resultado da soma 
+entre duas matrizes.*/
+
+#include <stdio.h>
 #define MAX 100
-int main()
-{
-    int m1[MAX][MAX]={{1,1,1},{2,2,2},{3,3,3,},{4,4,4},{5,5,5}};
-    int m2[MAX][MAX]={{5,5,5},{4,4,4},{3,3,3,},{2,2,2},{1,1,1}};
-    somanxn(m1,m2,5,3,5,3);
+
+void imprimir_soma(int mat1[MAX][MAX],int mat2[MAX][MAX], int imat1, int jmat1, int imat2, int jmat2);
+
+int main(){
+    int mat1[MAX][MAX] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    int mat2[MAX][MAX] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    imprimir_soma(mat1, mat2, 5,3,5,3);
     return 0;
 }
 
-void somanxn(int a[MAX][MAX], int b[MAX][MAX], int fa, int ca, int fb, int cb){
-    int fila, coluna;
-    if (fa != fb || ca !=cb){
-        printf ("As matrizes não tem a mesma ordem");
+void imprimir_soma(int mat1[MAX][MAX],int mat2[MAX][MAX], int imat1, int jmat1, int imat2, int jmat2){ 
+    int i, j;
+    if(imat1 != imat2 || jmat1 != jmat2){
+        printf("As matrizes não tem a mesma ordem");
         return;
     }
-    for (filas=0;fila<fa;fila++){
-        printf ("\n");
-        for (coluna=0;coluna<ca; coluna++){
-            printf (" %d", a[fila][coluna]+b[fila][coluna]);
+    printf("Matriz soma:\n");
+    for (i=0; i<imat1; i++){
+        printf("\n");
+        for(j=0; j<jmat1; j++){
+            printf("%d ", mat1[i][j]+mat2[i][j]);
         }
+       
     }
 }
