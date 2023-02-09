61655e9ad09400c17232b839a24689d1e6a51c03
alicesophi <120331753+alicesophi@users.noreply.github.com>
2023-02-08 22:42:38 -0400

    Update ex4.c

--- aula 26.01/ex4.c
+++ aula 26.01/ex4.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
 
-
 void encontrar_n(int m1[3][3],int numero);
 
 int main(){
     int m1[3][3];
-    int i, j, k, result;
+    int i, j, k;
    
-    
     printf("Digite valor para os elementos da matriz\n");
     
     for(i=0; i<3; i++){
         for(j=0; j<3; j++){
             printf("Matriz 1 - Elemento[%d][%d] = ", i, j);
             scanf("%d", &m1[i][j]);
-            
-            //printf("%d", result[i][j]);
-            
-            
         }
     }
     
-    printf("Digite um número que vc deseja encontrar nos elementos: ");
+    printf("Digite um número que você deseja encontrar nos elementos: ");
     scanf("%d", &k);
     
     encontrar_n(m1, k);
@@ -31,14 +25,17 @@ int main(){
 
 void encontrar_n(int m1[3][3],int numero){
     int i, j;
+    int encontrado = 0;
     for(i=0; i<3; i++){
         for(j=0; j<3; j++){
             if (m1[i][j] == numero){
-                printf("O numero foi encontrado no elemento m[%d][%d]", i, j);
-            }
-            else{
-                printf("Nao encontrado"); 
+                printf("O número foi encontrado no elemento m[%d][%d]\n", i, j);
+                encontrado = 1;
+                break;
             }
         }
+    }
+    if (encontrado==0) { //ou if(!encontrado)
+        printf("Não encontrado\n");
     }
 }
