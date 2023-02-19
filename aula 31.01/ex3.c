c174d259ce90b51f7d48359438992fb1b117ca36
alicesophi <120331753+alicesophi@users.noreply.github.com>
2023-02-19 18:32:32 -0400

    Update ex3.c

--- aula 31.01/ex3.c
+++ aula 31.01/ex3.c
@@ -1,40 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#define TAM 10
-
-int verifica_data(char data[]);
-
-int main() {
-    char data[TAM];
-    int dia, mes, ano;
-    
-    printf("Insira uma data no formato DD/MM/AAAA: ");
-    scanf("%s", data);
-    
-    if (!verifica_data(data)) {
-        printf("Data inválida!\n");
+int main(){
+	char data[10];
+	int dia=0, mes=0, ano=0;
+	int i;
+	
+	printf("Digite uma data no formato 'DD/MM/AAAA': ");
+	scanf("%s", data);
+	
+	//verificar se as barras estão corretas:
+	
+	while(data[i] != '\0'){
+		if(i==2 || i==5){
+			if(data[i] != '/'){
+				printf("data invalida.\n");
+				return 0;
+			}
+		}
+		else if(data[i] < '0' || data[i] > '9'){
+			printf("Data invalida.\n");
+			return 0;
+		}
+		i++;
+	}
+	
+	// extrair o dia mes e ano da data
+	
+	dia = (data[0] - '0')*10 + (data[1] - '0');
+	mes = (data[3] - '0')*10 + (data[4] - '0');
+	ano = (data[6] - '0')*1000 + (data[7] - '0')*100 + (data[8] - '0')*10 + (data[9] - '0')*1;
+	
+	//verificar se os valores sao validos
+	
+	if(dia < 1 || dia > 31 || mes < 1 || mes > 12 || ano < 0) {
+        printf("Data invalida.\n");
         return 0;
     }
-    
-    dia = (data[0] - '0') * 10 + (data[1] - '0');
-    mes = (data[3] - '0') * 10 + (data[4] - '0');
-    ano = (data[6] - '0') * 1000 + (data[7] - '0') * 100 + (data[8] - '0') * 10 + (data[9] - '0');
-    
-    printf("Dia: %d\n", dia);
-    printf("Mês: %d\n", mes);
+	
+	printf("Dia: %d\n", dia);
+    printf("Mes: %d\n", mes);
     printf("Ano: %d\n", ano);
-    
-    return 0;
-}
 
-int verifica_data(char data[]) {
-    int i;
-    if (strlen(data) != 10) return 0;
-    if (data[2] != '/' || data[5] != '/') return 0;
-    for (i = 0; i < 10; i++) {
-        if (i == 2 || i == 5) continue;
-        if (data[i] < '0' || data[i] > '9') return 0;
-    }
-    return 1;
+	return 0;
 }
