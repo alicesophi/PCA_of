4a3ccaa0343dfa85e40119761d68049ee39be1de
alicesophi <120331753+alicesophi@users.noreply.github.com>
2023-03-23 10:19:00 -0400

    Update ex008.c

--- aula 07.03/ex008.c
+++ aula 07.03/ex008.c
@@ -1,21 +1,22 @@
-#include <stdio.h>
-
-void troca(float *a, float *b);
+void troca(float *a,float *b);
 
 int main(){
-	float x=2.5;
-	float y=4.5;
+	float x = 2.5;
+	float y = 4.5;
+	
+	printf("Antes da troca: x = %.1f, y = %.1f", x, y);
 	
 	troca(&x, &y);
-	printf("x = %.1f, y = %.1f", x, y);
 	
+	printf("\nDepois da troca: x = %.1f, y = %.1f", x, y);
 	return 0;
 }
 
 void troca(float *a, float *b){
-	float temp = *a;
+	float temp;
 	
+	temp = *a;
 	*a = *b;
 	*b = temp;
-		
+	
 }
